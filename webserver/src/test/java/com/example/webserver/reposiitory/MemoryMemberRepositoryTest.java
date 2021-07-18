639346fed08aaa2387df1be39ae0f53fa17a7481
SeokHyunKho <seokhyunkho@gmail.com>
2021-07-18 15:51:53 +0900

    Inflearn 10. Domain Test

---
+++ webserver/src/test/java/com/example/webserver/reposiitory/MemoryMemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.webserver.reposiitory;
+
+import com.example.webserver.domain.Member;
+import com.example.webserver.repository.MemberMemoryRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemberMemoryRepository repository = new MemberMemoryRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+    }
+}
